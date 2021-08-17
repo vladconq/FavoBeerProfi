f9766152c4561ecb4954e84f1327421cfda619cc
Vladislav Bogucharov <vladconq@gmail.com>
2021-08-17 07:46:49 +0300

    Add protocol for updating table

---
+++ FavoBeerPro/Utils/UpdateAllBeersTableDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  UpdateAllBeersTableDelegate.swift
+//  FavoBeerPro
+//
+//  Created by Vladislav on 17.08.2021.
+//
+
+import Foundation
+
+protocol UpdateAllBeersTableDelegate {
+    func updateView()
+}
